22931202e6903e8d742bdd0fe76f2f45be94c0bc
stas-shr <stasshr@gmail.com>
2023-05-24 17:34:03 +0300

    Native avatar

--- Native_Avatar/ViewController.swift
+++ Native_Avatar/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    let imageSize: CGFloat = 36
+    
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .systemBackground
+        return scrollView
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let image = UIImage(systemName: "person.crop.circle.fill")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.tintColor = .gray
+        return imageView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Avatar"
+        view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        scrollView.contentSize = .init(width: view.frame.width, height: 2000)
+        scrollView.delegate = self
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view = scrollView
+        imageView.contentMode = .scaleAspectFill
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        guard let subviews = navigationController?.navigationBar.subviews else {
+            return
+        }
+        
+        for subview in subviews {
+            if let label = subview.subviews.first(where: { $0 is UILabel }) {
+                subview.addSubview(imageView)
+                
+                NSLayoutConstraint.activate([
+                    imageView.heightAnchor.constraint(equalToConstant: imageSize),
+                    imageView.widthAnchor.constraint(equalToConstant: imageSize),
+                    imageView.trailingAnchor.constraint(equalTo: subview.trailingAnchor, constant: -16),
+                    imageView.centerYAnchor.constraint(equalTo: label.centerYAnchor)
+                ])
+            }
+        }
+    }
 }
-
